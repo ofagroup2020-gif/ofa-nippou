899c8d988951209b256ab87c1956d8ffc7cbeb17
ofagroup2020-gif <ofa.group2020@gmail.com>
2026-01-02 02:44:58 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+// config.js
+window.OFA = {
+  // ★あなたの最新WebApp URL（/exec まで）
+  GAS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbxAJa6S3-t-MHs4BraAYWqkW8mVi3QyPNRmCybCKbOrpmBn6HefQND3BBHCVYRcu0bfAg/exec",
+
+  // ★本物のGoogle Client ID
+  GOOGLE_CLIENT_ID: "321435608721-vfrb8sgjnkqake7rgrscv8de798re2tl.apps.googleusercontent.com",
+
+  // アプリ識別キー
+  APP_KEY: "OFA_TENKO",
+
+  // ローカル保存キー
+  LS_TOKEN: "ofa_id_token",
+  LS_EMAIL: "ofa_email",
+  LS_NAME: "ofa_name",
+  LS_ROLE: "ofa_role" // "driver" | "admin"
+};
