ac98dc08826c1d3532e02ac8833a6b9c1e299eb3
ofagroup2020-gif <ofa.group2020@gmail.com>
2026-01-05 02:48:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 // config.js
-window.OFA_CFG = {
-  GAS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbzhROZ8ZJ_ERv5m430lbJ3h_4C8g7E9x2oOJUI4AbRcasRGi-EsNnCKyHSglc1jp0uTCA/exec",
-  DRIVER_PASS: "202601",
-  ADMIN_PASS: "ofa-2026",
-  APP_KEY: "OFA_TENKO"
-};
+const GAS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwY_n12SABO4nvwpoyNZU8AVepuLtogwer_D1hEmvS_p5KQiq32MvJ0FaRPOBI6khbvnQ/exec";
+const SPREADSHEET_ID = "1-TG329eacLtCoi-htKQu7cY0Y0MFoPRTk_fTvr3hmOQ";
+
+const OFA_DRIVER_PASSWORD = "202601";
+const OFA_ADMIN_PASSWORD  = "ofa-2026";
